3121fa38761592acef626510cbd5fa39f2588762
Mka <mka281@icloud.com>
2018-01-23 00:32:29 +0300

    Fix by adding parantheses

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -99,10 +99,12 @@ $(function() {
            expect(entryNum).toBeGreaterThan(0);
            done();
          });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
 }());
